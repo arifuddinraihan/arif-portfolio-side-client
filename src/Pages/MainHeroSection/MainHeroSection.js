2a12fdf7a6a112e0c615b5a22cb2352a22f27936
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-12-11 15:53:18 +0600

    added all warning fix

--- src/Pages/MainHeroSection/MainHeroSection.js
+++ src/Pages/MainHeroSection/MainHeroSection.js
@@ -6,18 +6,18 @@ import ResumeModal from './ResumeModal/ResumeModal';
 
 const MainHeroSection = () => {
     return (
-        <div class="px-6 py-8">
-            <div class="flex flex-col items-center py-6 lg:h-[36rem] lg:flex-row xl:min-h-screen">
-                <div class="">
-                    <h2 class="flex items-end text-lg md:text-xl lg:text-3xl font-semibold underline text-primary dark:text-slate-200">
+        <div className="px-6 py-8">
+            <div className="flex flex-col items-center py-6 lg:h-[36rem] lg:flex-row xl:min-h-screen">
+                <div className="">
+                    <h2 className="flex items-end text-lg md:text-xl lg:text-3xl font-semibold underline text-primary dark:text-slate-200">
                         Arif Uddin Raihan
                         <label htmlFor="resume-modal" className='btn btn-sm btn-ghost hover:bg-transparent'>
                             <small className='flex items-end text-sm md:text-md lg:text-2xl font-semibold underline text-blue-500 dark:text-blue-400 ml-4 gap-2 lowercase'> <VscGoToFile></VscGoToFile> resume</small>
                         </label>
                     </h2>
-                    <h2 class="text-xl md:text-2xl font-normal text-black lg:text-5xl xl:text-6xl py-8 xl:py-16 dark:text-base-100">
+                    <h2 className="text-xl md:text-2xl font-normal text-black lg:text-5xl xl:text-6xl py-8 xl:py-16 dark:text-base-100">
                         Arif is a web developer who is committed to advancing his career in the developer industry by creating thoughtful visual identities and memorable experiences for web users.</h2>
-                    <h2 class="text-md md:text-lg font-light text-black lg:text-2xl font-mono dark:text-base-100">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</h2>
+                    <h2 className="text-md md:text-lg font-light text-black lg:text-2xl font-mono dark:text-base-100">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</h2>
                 </div>
                 <ResumeModal></ResumeModal>
             </div>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,25 +3,25 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <footer class="flex justify-center px-4 text-gray-800 dark:text-white dark:bg-gray-900">
-                <div class="container px-6 py-6">
-                    <h1 class="text-lg font-bold text-center lg:text-2xl">
+            <footer className="flex justify-center px-4 text-gray-800 dark:text-white dark:bg-gray-900">
+                <div className="container px-6 py-6">
+                    <h1 className="text-lg font-bold text-center lg:text-2xl">
                         Join 31,000+ other and never miss <br /> out on new tips, tutorials, and more.
                     </h1>
 
-                    <hr class="h-px bg-gray-200 border-none my-7 dark:bg-gray-700" />
+                    <hr className="h-px bg-gray-200 border-none my-7 dark:bg-gray-700" />
 
-                    <div class="flex flex-col items-center justify-between md:flex-row">
+                    <div className="flex flex-col items-center justify-between md:flex-row">
                         <div>
-                            <a href="#" class="text-2xl font-bold text-gray-800 transition-colors duration-300 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">Brand</a>
+                            <a href="#" className="text-2xl font-bold text-gray-800 transition-colors duration-300 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">Brand</a>
                         </div>
 
-                        <div class="flex mt-4 md:m-0">
-                            <div class="-mx-4">
-                                <a href="#" class="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">About</a>
-                                <a href="#" class="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">Blog</a>
-                                <a href="#" class="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">News</a>
-                                <a href="#" class="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">Contact</a>
+                        <div className="flex mt-4 md:m-0">
+                            <div className="-mx-4">
+                                <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">About</a>
+                                <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">Blog</a>
+                                <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">News</a>
+                                <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">Contact</a>
                             </div>
                         </div>
                     </div>
